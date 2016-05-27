31cf69fb68d2c085615c13740d90fe6725dffa22
Joao Marcelo Barros <joaomarcelobarros@gmail.com>
2016-05-26 23:23:39 -0300

    install rundeck

--- install_rundeck.sh
+++ install_rundeck.sh
@@ -2,11 +2,9 @@
 #script by jmbarros at tech4it.com.br
 #install rundeck
 
-ufw disable
-apt-get remove ufw
-apt-get purge ufw
-
-apt-get update
-apt-get install wget openjdk-7-jdk
+apt-get update -y
+apt-get install wget openjdk-7-jdk -y
 wget http://dl.bintray.com/rundeck/rundeck-deb/rundeck-2.6.7-1-GA.deb
 dpkg -i rundeck-2.6.7-1-GA.deb
+/etc/init.d/rundeckd restart
+chkconfig rundeckd on 
